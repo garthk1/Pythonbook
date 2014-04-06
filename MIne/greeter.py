19b7b0b3cf5b0bbc836d97d62eb9a6bb5b2e390a
Garth <gkline@line2.com>
2014-04-05 17:49:47 -0700

    New project

---
+++ MIne/greeter.py
@@ -0,0 +1,9 @@
+# Greeter
+# Demonstrates the use of a variable
+
+name = "Larry"
+
+print(name)
+print ("Hi,", name)
+
+input("\n\nPress the enter key to exit.")
---
+++ MIne/personal_greeter.py
@@ -0,0 +1,9 @@
+# Personal Greeter __author__ = 'gkline'
+# Demonstrates getting user input and variables..aslo
+
+name = input("Hi. What's your name? ")
+
+print(name)
+print("Hi,",name)
+
+input("\n\nPress the enter key to exit.")
